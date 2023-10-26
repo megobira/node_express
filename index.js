58720a75b102666905d3c13da9f240f0cc8d11fb
megobira@gmail.com <maria_gobira@educadventista.org>
2023-10-26 15:02:25 -0300

    recebendo parametros na rota

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+// http://localhost:3000/usuarios/20
+
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuario ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
